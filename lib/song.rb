f62a8b62569f2d71b0feb4a1cba1431eba4b1902
Travis Exline <t.exline22@gmail.com>
2019-05-20 14:26:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,11 @@
 class Song
   attr_accessor :name, :artist, :genre
+  @@count = 0
 
   def initialize(new_name, new_artist, new_genre)
     @name = new_name
     @artist = new_artist
     @genre = new_genre
+    @@count += 1
   end
 end
