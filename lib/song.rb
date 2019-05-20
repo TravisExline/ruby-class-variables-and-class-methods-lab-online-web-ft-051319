d18831e64b89886651283c140362e9c42630a99a
Travis Exline <t.exline22@gmail.com>
2019-05-20 14:23:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,2 +1,9 @@
 class Song
+  attr_accessor :name, :artist, :genre
+
+  def initialize(new_name, new_artist, new_genre)
+    @name = new_name
+    @artist = new_artist
+    @genre = new_genre
+  end
 end
